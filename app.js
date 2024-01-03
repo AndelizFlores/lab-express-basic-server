5a5e6cdaf9d1d7092a2e1f2f22ac699d14bca088
Andeliz Flores <andelizflores@outlook.com>
2024-01-03 12:03:19 -0400

    Lab Express Basic Server v1

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 // IMPORT PACKAGES
 // Here you should import the required packages for your Express app: `express` and `morgan`
+require('dotenv').config()
 
+var express = require('express')
+var logger = require('morgan')
 
+var apiRouter = require('./routes/api')
 
 // CREATE EXPRESS APP
 // Here you should create your Express app:
+var app = express()
 
+// Set up the following middleware that you will need for your Express server:
+app.use(express.static('public'))
+app.use(express.json())
+app.use(logger('dev'))
 
+app.get('/', (req, res, next) => {
 
-// MIDDLEWARE
-// Here you should set up the required middleware:
-// - `express.static()` to serve static files from the `public` folder
-// - `express.json()` to parse incoming requests with JSON payloads
-// - `morgan` logger to log all incoming requests
+    res.sendFile(__dirname + '/views/home.html')
+
+})
+
+app.get('/blog', (req, res, next) =>{
+    res.sendFile(__dirname + '/views/blog.html')
+})
+
+app.use('/api', apiRouter)
 
 
 
@@ -23,3 +37,7 @@
 
 // START THE SERVER
 // Make your Express server listen on port 5005:
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is listening on " + process.env.PORT)
+})
